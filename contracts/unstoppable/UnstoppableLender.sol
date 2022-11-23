58a11d902c7280a6fcf0243eb8f14e5a7e77c8a6
楊茂榮 <andy88566y@gmail.com>
2022-11-23 16:00:09 +0800

    complete unstoppable

--- contracts/unstoppable/UnstoppableLender.sol
+++ contracts/unstoppable/UnstoppableLender.sol
@@ -14,7 +14,6 @@ interface IReceiver {
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract UnstoppableLender is ReentrancyGuard {
-
     IERC20 public immutable damnValuableToken;
     uint256 public poolBalance;
 
@@ -27,10 +26,16 @@ contract UnstoppableLender is ReentrancyGuard {
         require(amount > 0, "Must deposit at least one token");
         // Transfer token from sender. Sender must have first approved them.
         damnValuableToken.transferFrom(msg.sender, address(this), amount);
+        // 下面這行沒有用 safeMath 會 overflow?
+        // 不對， compiler version 是 0.8.0 應該沒問題
         poolBalance = poolBalance + amount;
+        // 要使用這個 function 才會 update poolBalance
+        // 但是 token.transfer(address(pool), 1) 卻不會
+        // 然後下面的 assert(poolBalance == balanceBefore); 就壞惹，永遠不會過
     }
 
     function flashLoan(uint256 borrowAmount) external nonReentrant {
+        // denial of service
         require(borrowAmount > 0, "Must borrow at least one token");
 
         uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
@@ -38,12 +43,18 @@ contract UnstoppableLender is ReentrancyGuard {
 
         // Ensured by the protocol via the `depositTokens` function
         assert(poolBalance == balanceBefore);
-        
+
         damnValuableToken.transfer(msg.sender, borrowAmount);
-        
-        IReceiver(msg.sender).receiveTokens(address(damnValuableToken), borrowAmount);
-        
+
+        IReceiver(msg.sender).receiveTokens(
+            address(damnValuableToken),
+            borrowAmount
+        );
+
         uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
-        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+        require(
+            balanceAfter >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
 }
